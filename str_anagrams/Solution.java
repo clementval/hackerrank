4aaad2cb0a76b960c8596435df280bc68e2c9779
Valentin Clement <clementval@gmail.com>
2016-10-11 19:01:48 +0200

    Push working solution with frequence counting

--- str_anagrams/Solution.java
+++ str_anagrams/Solution.java
@@ -3,9 +3,42 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+
 public class Solution {
+
     public static int numberNeeded(String first, String second) {
-        return 0;      
+        int needed = 0;
+
+        Map<Character, Integer> charMap = new HashMap<>();
+
+        for(int i = 0; i < first.length(); i++){
+            char c = first.charAt(i);
+            if(charMap.containsKey(c)){
+                charMap.put(c, charMap.get(c) + 1);
+            } else {
+                charMap.put(c, 1);
+            }
+        }
+
+        for(int i = 0; i < second.length(); i++){
+            char c = second.charAt(i);
+            if(charMap.containsKey(c)){
+                charMap.put(c, charMap.get(c) - 1);
+                if(charMap.get(c) == 0){
+                    charMap.remove(c);
+                }
+            } else {
+                ++needed;
+            }
+        }
+
+        Iterator it = charMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            needed = needed + (int)pair.getValue();
+        }
+
+        return needed;
     }
   
     public static void main(String[] args) {
