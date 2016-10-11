22fc84487ef05745924659a9ed9a59ee834bd328
Valentin Clement <clementval@gmail.com>
2016-10-11 18:06:42 +0200

    Push solution

---
+++ array_rotation/Solution.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        rotate_array(a, n, k);
+        print_array(a);
+    }
+
+    private static void rotate_array(int[] a, int n, int k) {
+        if(k > n) {
+          k = k % n;
+        }
+        reverse_array(a, 0, n-1);
+        reverse_array(a, 0, k-1);
+        reverse_array(a, k, n-1);
+    }
+
+    private static void reverse_array(int[] a, int left, int right) {
+        while (left < right) {
+            int temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+            left++;
+            right--;
+        }
+    }
+ 
+    private static void print_array(int[] a){
+        for(int i : a){
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+    }
+
+}
+
