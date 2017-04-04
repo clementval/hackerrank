ea2d336bde9ff3c8389fc12510ccc1118f07d754
Valentin Clement <clementval@gmail.com>
2017-04-04 16:21:54 +0200

    Solution (BST Insertion)

---
+++ data_structure/tree/bst_insertion/Solution.java
@@ -0,0 +1,37 @@
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+static Node Insert(Node root,int value)
+{
+    Node inserted = new Node();
+    inserted.data = value;
+    Node crt = root;
+    boolean notInserted = true;
+    while(notInserted){
+        if(value > crt.data){
+            if(crt.right != null){
+                crt = crt.right;
+            } else {
+                crt.right = inserted;
+                notInserted = false;
+            }
+        } else {
+            if(crt.left != null){
+                crt = crt.left;
+            } else {
+                crt.left = inserted;
+                notInserted = false;
+            }
+        }
+    }
+    return root;
+}
+
+
+
+
