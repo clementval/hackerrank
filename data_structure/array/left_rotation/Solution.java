9245a2dd76618fab5789673798e672552f9e811f
Valentin Clement <clementval@gmail.com>
2017-04-10 17:05:14 +0200

    Solution left rotation

---
+++ data_structure/array/left_rotation/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; ++i){
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0; i < n; ++i) {  
+            System.out.print(arr[(i+d)%n] + " "); 
+        }
+	System.out.println("");
+    }
+}
+
