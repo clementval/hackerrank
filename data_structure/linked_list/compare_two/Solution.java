7e1ab676d0804d0888b00481d9ae74fc6dfd2c7d
Valentin Clement <clementval@gmail.com>
2017-04-10 19:11:23 +0200

    Partial Solution: Compare two linked lists

---
+++ data_structure/linked_list/compare_two/Solution.java
@@ -0,0 +1,28 @@
+/*
+  Compare two linked lists A and B
+  Return 1 if they are identical and 0 if they are not. 
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+int CompareLists(Node headA, Node headB) {
+    if(headA == null || headB == null){
+        return 0;
+    }
+    
+    while(headA != null && headB != null){
+        if(headA.data != headB.data){
+            return 0;
+        }
+        headA = headA.next;
+        headB = headB.next;
+    }
+    
+    if(headA == null && headB == null)
+        return 1;
+    return 0;
+}
+
+
