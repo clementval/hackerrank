9a907fed1c876ff8c1cd8ae8ff75ecd5d5b2a9cf
Valentin Clement <clementval@gmail.com>
2017-04-10 16:35:14 +0200

    Insertion Sort Part 2

---
+++ algo/insertion_part2/Solution.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void insertionSortPart2(int[] input) {    
+        int temp;
+        for (int i = 1; i < input.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(input[j] < input[j-1]){
+                    temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                }
+            }
+            printArray(input);
+        }
+    }  
+    
+    
+      
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       int s = in.nextInt();
+       int[] ar = new int[s];
+       for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+       }
+       insertionSortPart2(ar);    
+                    
+    }    
+    private static void printArray(int[] ar) {
+      for(int n: ar){
+         System.out.print(n+" ");
+      }
+        System.out.println("");
+   }
+}
+
